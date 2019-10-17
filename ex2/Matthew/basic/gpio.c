91e630db7fd99ec9c3ab4d64a791ca824e3d0c52
Matěj Mňouček <matej.mnoucek@gmail.com>
2019-10-17 15:54:36 +0200

    Basic version done

--- ex2/Matthew/basic/gpio.c
+++ ex2/Matthew/basic/gpio.c
@@ -3,16 +3,11 @@
 
 #include "efm32gg.h"
 
-
 /*
  * function to set up GPIO mode and interrupts
  */
 void setupGPIO()
 {
-	/*
-	* TODO set input and output pins for the joystick 
-	*/
-
 	// GPIO clock
 	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;
 
@@ -24,21 +19,7 @@ void setupGPIO()
 	// Quick test = Turn on LEDs D4-D8
 	//*GPIO_PA_DOUT = 0x0700;
 
-
 	// Buttons
 	*GPIO_PC_MODEL = 0x33333333; // Set pins A0-7 as input
 	*GPIO_PC_DOUT = 0xff; // Enable internal pull-ups
-
-	// Quick test = Pooling of buttons and LEDs
-	/*
-	while (true)
-	{
-		*GPIO_PA_DOUT = (*GPIO_PC_DIN << 8);
-	}
-	*/
-
-	/*
-	 * Example of HW access from C code: turn on joystick LEDs D4-D8 check 
-	 * efm32gg.h for other useful register definitions 
-	 */
 }
