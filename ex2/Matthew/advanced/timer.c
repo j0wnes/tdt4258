5d1c87d0e8df68eb38af1b756886920c1d1ab799
Matěj Mňouček <matej.mnoucek@gmail.com>
2019-10-17 14:47:46 +0200

    WiP basic version

---
+++ ex2/Matthew/advanced/timer.c
@@ -0,0 +1,41 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "efm32gg.h"
+
+/*
+ * function to setup the timer 
+ */
+void enableTimer(uint16_t period)
+{
+	/*
+	 * TODO enable and set up the timer
+	 * 
+	 * 1. Enable clock to timer by setting bit 6 in CMU_HFPERCLKEN0 2.
+	 * Write the period to register TIMER1_TOP 3. Enable timer interrupt
+	 * generation by writing 1 to TIMER1_IEN 4. Start the timer by writing 
+	 * 1 to TIMER1_CMD
+	 * 
+	 * This will cause a timer interrupt to be generated every (period)
+	 * cycles. Remember to configure the NVIC as well, otherwise the
+	 * interrupt handler will not be invoked. 
+	 */
+
+	// Enable clock to timer
+	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;
+	
+	// Write the period to register
+	*TIMER1_TOP = period;
+	
+	// Start the timer
+	*TIMER1_CMD = 1;
+}
+
+void disableTimer()
+{
+	// Disable clock to timer
+	*CMU_HFPERCLKEN0 &= ~CMU2_HFPERCLKEN0_TIMER1;
+
+	// Stop the timer
+	*TIMER1_CMD = 0;
+}
